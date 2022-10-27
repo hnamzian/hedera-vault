81d3503a6e2e3839d0c6cfe4b19fe69fffcfc4c7
hnamzian <namazian87@gmail.com>
2022-10-27 11:56:53 +0300

    Create KeyHandler that manages Paths to key operations

---
+++ handlers/key/key_handler.go
@@ -0,0 +1,106 @@
+package key
+
+import (
+	"context"
+
+	"github.com/hashicorp/errwrap"
+	"github.com/hashicorp/vault/sdk/framework"
+	"github.com/hashicorp/vault/sdk/logical"
+)
+
+type KeyHandler struct {
+	// logger *log.Logger
+}
+
+func NewKeyHandler() *KeyHandler {
+	return &KeyHandler{}
+}
+
+func (h KeyHandler) Paths() []*framework.Path {
+	return []*framework.Path{
+		{
+			Pattern: "keys/?",
+
+			Fields: map[string]*framework.FieldSchema{
+				"path": {
+					Type:        framework.TypeString,
+					Description: "Specifies the path of the secret.",
+				},
+				"id": {
+					Type: framework.TypeString,
+				},
+				"algo": {
+					Type: framework.TypeString,
+				},
+				"curve": {
+					Type: framework.TypeString,
+				},
+			},
+
+			Operations: map[logical.Operation]framework.OperationHandler{
+				logical.CreateOperation: &framework.PathOperation{
+					Callback: h.handleWrite,
+				},
+				logical.ReadOperation: &framework.PathOperation{
+					Callback: h.handleRead,
+				},
+				logical.ListOperation: &framework.PathOperation{
+					Callback: h.handleList,
+				},
+				logical.DeleteOperation: &framework.PathOperation{
+					Callback: h.handleDelete,
+				},
+			},
+
+			ExistenceCheck: h.handleExistenceCheck,
+		},
+	}
+	// return framework.PathAppend(
+	// 	[]*framework.Path{
+	// 		{
+	// 			Pattern: "keys/?",
+
+	// 			Fields: map[string]*framework.FieldSchema{
+	// 				"path": {
+	// 					Type:        framework.TypeString,
+	// 					Description: "Specifies the path of the secret.",
+	// 				},
+	// 				"id": {
+	// 					Type: framework.TypeString,
+	// 				},
+	// 				"algo": {
+	// 					Type: framework.TypeString,
+	// 				},
+	// 				"curve": {
+	// 					Type: framework.TypeString,
+	// 				},
+	// 			},
+
+	// 			Operations: map[logical.Operation]framework.OperationHandler{
+	// 				logical.CreateOperation: &framework.PathOperation{
+	// 					Callback: h.handleWrite,
+	// 				},
+	// 				logical.ReadOperation: &framework.PathOperation{
+	// 					Callback: h.handleRead,
+	// 				},
+	// 				logical.ListOperation: &framework.PathOperation{
+	// 					Callback: h.handleList,
+	// 				},
+	// 				logical.DeleteOperation: &framework.PathOperation{
+	// 					Callback: h.handleDelete,
+	// 				},
+	// 			},
+	// 		},
+	// 	},
+	// )
+}
+
+func (h *KeyHandler) handleExistenceCheck(ctx context.Context, req *logical.Request, data *framework.FieldData) (bool, error) {
+	// b.Logger().Debug("Handle Check", req.Path)
+	out, err := req.Storage.Get(ctx, req.Path)
+	if err != nil {
+		return false, errwrap.Wrapf("existence check failed: {{err}}", err)
+	}
+
+	return out != nil, nil
+}
