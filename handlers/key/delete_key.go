5b0388d527e20301f4dc99725a81a69bb9086ea8
hnamzian <namazian87@gmail.com>
2022-10-27 11:55:56 +0300

    Create Dellete handler for Keys by request

---
+++ handlers/key/delete_key.go
@@ -0,0 +1,32 @@
+package key
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/vault/sdk/framework"
+	"github.com/hashicorp/vault/sdk/logical"
+	"github.com/hnamzian/hedera-vault-plugin/storage"
+)
+
+func (h *KeyHandler) handleDelete(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
+	if req.ClientToken == "" {
+		return nil, fmt.Errorf("client token empty")
+	}
+
+	path := data.Get("path").(string)
+	id := data.Get("id").(string)
+
+	// Remove entry for specified path
+	if err := storage.
+		NewStorage(req).
+		WithContext(ctx).
+		WithKey(req.ClientToken, path, id).
+		Delete(); err != nil {
+		return nil, fmt.Errorf("delete key from storage failed %s", err)
+	}
+
+	// h.logger.Debug("Handle Update", "data", data, "\nreq", req)
+
+	return nil, nil
+}
