c2c80593742b4f3f24185c432e28b829b070857d
hnamzian <namazian87@gmail.com>
2022-10-27 11:56:18 +0300

    Create List handler for Keys by request

---
+++ handlers/key/list_key.go
@@ -0,0 +1,32 @@
+package key
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/vault/sdk/framework"
+	"github.com/hashicorp/vault/sdk/logical"
+	"github.com/hnamzian/hedera-vault-plugin/storage"
+)
+
+func (h *KeyHandler) handleList(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
+	if req.ClientToken == "" {
+		return nil, fmt.Errorf("client token empty")
+	}
+
+	path := data.Get("path").(string)
+
+	keys, err := storage.NewStorage(req).WithContext(ctx).WithKey(req.ClientToken, path, "").List()
+	if err != nil {
+		return nil, err
+	}
+	if keys == nil {
+		resp := logical.ErrorResponse("No value at %v%v", req.MountPoint, path)
+		return resp, nil
+	}
+
+	// Generate the response
+	resp := logical.ListResponse(keys)
+
+	return resp, nil
+}
