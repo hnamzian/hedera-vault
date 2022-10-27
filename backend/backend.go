c7ddc15ce2bf19b7b39fb25100a22505d76b44d2
hnamzian <namazian87@gmail.com>
2022-10-27 11:57:12 +0300

    Create backendd to manage requests

---
+++ backend/backend.go
@@ -0,0 +1,57 @@
+package backend
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/hashicorp/vault/sdk/framework"
+
+	"github.com/hashicorp/vault/sdk/logical"
+	"github.com/hnamzian/hedera-vault-plugin/handlers"
+)
+
+// backend wraps the backend framework and adds a map for storing key value pairs
+type backend struct {
+	*framework.Backend
+}
+
+var _ logical.Factory = Factory
+
+// Factory configures and returns Mock backends
+func Factory(ctx context.Context, conf *logical.BackendConfig) (logical.Backend, error) {
+	b, err := newBackend()
+	if err != nil {
+		return nil, err
+	}
+
+	if conf == nil {
+		return nil, fmt.Errorf("configuration passed into backend is nil")
+	}
+
+	if err := b.Setup(ctx, conf); err != nil {
+		return nil, err
+	}
+
+	return b, nil
+}
+
+func newBackend() (*backend, error) {
+	b := &backend{}
+
+	h := handlers.NewHandler()
+
+	b.Backend = &framework.Backend{
+		Help:        strings.TrimSpace(mockHelp),
+		BackendType: logical.TypeLogical,
+		Paths: framework.PathAppend(
+			h.Key.Paths(),
+		),
+	}
+
+	return b, nil
+}
+
+const mockHelp = `
+The Mock backend is a dummy secrets backend that stores kv pairs in a map.
+`
